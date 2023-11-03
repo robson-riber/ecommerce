d2296280e6ffb22c5e8580ea6aa8b512837cac3b
Robson Ribeiro <robson.riber777@gmail.com>
2023-11-03 16:52:47 -0300

    Até aula 117

--- site.php
+++ site.php
@@ -55,7 +55,6 @@ $app->get("/products/:desurl", function($desurl){
 	$page->setTpl("product-detail", [
 		'product' => $product->getValues(), 
 		'categories' => $product->getcategories()]);
-
 });
 
 
@@ -65,10 +64,62 @@ $app->get("/cart", function(){
 
 	$page = new Page();
 
-	$page->setTpl("cart");
-
+	$page->setTpl("cart", [
+		'cart' => $cart->getValues(),
+		'products' => $cart->getProducts()
+	]);
 });
 
+
+$app->get("/cart/:idproduct/add", function($idproduct){
+
+	$product = new Product();
+
+	$product->get((int) $idproduct);
+
+	$cart = Cart::getFromSession();
+
+	var_dump($cart);
+	exit;
+	
+	$cart->teste();
+	exit;
+
+	//$cart->addProduct($product);
+
+	header("Location: /cart");
+	exit;
+});
+
+
+$app->get("/cart/:idproduct/minus", function($idproduct){
+
+	$product = new Product();
+
+	$product->get((int) $idproduct);
+
+	$cart = Cart::getFromSession();
+
+	$cart->removeProduct($product);
+
+	header("Location: /cart");
+	exit;
+});
+
+
+$app->get("/cart/:idproduct/remove", function($idproduct){
+
+	$product = new Product();
+
+	$product->get((int) $idproduct);
+
+	$cart = Cart::getFromSession();
+
+	$cart->removeProduct($product, true);
+
+	header("Location: /cart");
+	exit;
+});
 
 
 
